e36f6cb1bfd11c430f7c8290064bea8c69343cd6
krozark <maxime.barbier1991@gmail.com>
2012-06-16 20:43:07 -0400

    update

--- website/views.py
+++ website/views.py
@@ -2,7 +2,17 @@
 
 from django.views.generic import ListView, TemplateView, DetailView
 from website.models import *
-
+from Kraggne.models import MenuItem
+from django.shortcuts import get_object_or_404
 
 class HomeView(TemplateView):
-    template_name = 'website/home.html'
+    template_name = "Kraggne/genericPage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        context['page'] = get_object_or_404(MenuItem,slug='home')
+
+        return context
+
+
