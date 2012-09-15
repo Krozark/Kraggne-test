ea70b94bf420cf457158a42fd7398c734c944944
krozark <maxime.barbier1991@gmail.com>
2012-09-16 00:45:24 +0200

    up db

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ from website.views import *
 
 urlpatterns=patterns('',
     url(r'^$',          HomeView.as_view(),     name='website-home'),
+    url(r'^contentblocks/', include("Kraggne.contrib.contentblocks.urls")),
 )
